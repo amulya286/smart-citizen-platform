af9b5abb4bac7f8c1362c3d82b959f264f06e964
amulya286 <amulyanerti28@gmail.com>
2026-05-09 23:46:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,13 +8,26 @@ def home():
     return render_template('index.html')
 
 # LOGIN PAGE
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        return f"Welcome {username}"
+
     return render_template('login.html')
 
 # FEEDBACK PAGE
-@app.route('/feedback')
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
+
+    if request.method == 'POST':
+        message = request.form['feedback']
+
+        return "Feedback Submitted Successfully"
+
     return render_template('feedback.html')
 
 # RUN SERVER
